fe68c08bc1068d107e6cc8115eb81f80af8e32c5
James Lampton <jlampton@gmail.com>
2015-04-04 13:43:49 -0400

    W: refactored out base and started working on client.

--- src/main/docker/base/fetch.sh
+++ src/main/docker/base/fetch.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# Pull zookeeper
-pushd zookeeper
+echo Pull zk
 ZOOKEEPER_VERSION=3.4.6
 test -f zookeeper-$ZOOKEEPER_VERSION.tar.gz || curl -SsfLO "http://www.apache.org/dist/zookeeper/zookeeper-$ZOOKEEPER_VERSION/zookeeper-$ZOOKEEPER_VERSION.tar.gz"
-popd
 
-# Pull storm dependencies
-pushd storm
+echo Pull storm
 STORM_VERSION=0.9.3
 test -f apache-storm-$STORM_VERSION.tar.gz || curl -SsfLO "http://www.apache.org/dist/storm/apache-storm-$STORM_VERSION/apache-storm-$STORM_VERSION.tar.gz"
-popd
+
+echo Pull pig
+PIG_VERSION=0.13.0
+test -f pig-$PIG_VERSION.tar.gz || curl -SsfLO "http://www.apache.org/dist/pig/pig-$PIG_VERSION/pig-$PIG_VERSION.tar.gz"
+
+echo Pull Hadoop
--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker build -t piggybanksqueal/base base/
 docker build -t piggybanksqueal/zookeeper zookeeper/
 docker build -t piggybanksqueal/storm storm/
+docker build -t piggybanksqueal/client client/
 
